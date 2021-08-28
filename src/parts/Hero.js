a9ca2da1e9e454f9f5db3a0706adb5419f814cc1
avivsabilal29 <89498280+avivsabilal29@users.noreply.github.com>
2021-08-28 15:05:54 +0700

    adding reaact-reveal and bit transition each ssection

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
 import ImageHero from 'assets/images/img-hero.jpg'
 import ImageHero_ from 'assets/images/img-hero-frame.jpg'
 import IconCities from 'assets/images/icons/icon-cities.svg'
@@ -18,6 +19,7 @@ export default function Hero(props) {
 
 
     return (
+        <Fade bottom>
         <section className="container pt-4">
             <div className="row align-items-center">
                 <div className="col-auto pr-5" style={{ width: 530 }}>
@@ -64,5 +66,6 @@ export default function Hero(props) {
                 </div>
             </div>
         </section>
+        </Fade>
     )
 }
